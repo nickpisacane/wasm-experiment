f5e49c0d3dc1bafcdc7eb02d3c224b23362e83bd
Nick Pisacane <pisacanen@gmail.com>
2018-05-17 14:30:03 -0700

    add position, velocity, and acceleration to Object

---
+++ src/Object.cpp
@@ -0,0 +1,10 @@
+#include "Vector2.h"
+#include "Object.h"
+
+Object::Object() : Object(0, 0) {}
+
+Object::Object(int x, int y) :
+  position(x, y),
+  velocity(0, 0),
+  acceleration(0, 0) {
+}
--- src/Object.h
+++ src/Object.h
@@ -2,9 +2,16 @@
 #define __OBJECT_H__
 
 #include "Image.h"
+#include "Vector2.h"
 
 class Object {
 public:
+  Object();
+  Object(int x, int y);
+
+  Vector2 position;
+  Vector2 velocity;
+  Vector2 acceleration;
   virtual void draw(Image&) = 0;
 };
 
