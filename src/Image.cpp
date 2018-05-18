720cf176629457232516ca51d7427897f6c71b1b
Nick Pisacane <pisacanen@gmail.com>
2018-05-17 17:40:44 -0700

    fix index error

--- src/Image.cpp
+++ src/Image.cpp
@@ -34,12 +34,12 @@ bool Image::set(uint32_t x, uint32_t y, uint32_t rgba) {
     return false;
   }
 
-  uint32_t index = 4 * (x + y * this->_width);
+  uint32_t index = 4 * (this->_width * y + x);
   
-  this->_buffer[index] = rgba & 0xff000000 >> 24;
-  this->_buffer[index] = rgba & 0x00ff0000 >> 16;
-  this->_buffer[index] = rgba & 0x0000ff00 >> 8;
-  this->_buffer[index] = rgba & 0x000000ff;
+  this->_buffer[index + 0] = rgba & 0xff000000 >> 24;
+  this->_buffer[index + 1] = rgba & 0x00ff0000 >> 16;
+  this->_buffer[index + 2] = rgba & 0x0000ff00 >> 8;
+  this->_buffer[index + 3] = rgba & 0x000000ff;
 
   return true;
 }
